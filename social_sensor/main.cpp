fc1c893c0f94a04d6be80ce9349408718cd5fe1b
Jurim Lee <jurim0301@naver.com>
2018-04-28 11:16:43 +0900

    Initial Commit

---
+++ social_sensor/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  social_sensor
+//
+//  Created by Jurim Lee on 2018. 4. 28..
+//  Copyright © 2018년 Jurim Lee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
